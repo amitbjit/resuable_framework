a0e925c503f67ffb7aea0c2056ceabca2eb0a766
Amit Chowdhury <amit.chowdhury@bjitgroup.com>
2021-07-13 13:40:59 +0600

    init commit

---
+++ Reusable_framework/Reusable_framework.h
@@ -0,0 +1,18 @@
+//
+//  Reusable_framework.h
+//  Reusable_framework
+//
+//  Created by BJIT on 2021/07/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Reusable_framework.
+FOUNDATION_EXPORT double Reusable_frameworkVersionNumber;
+
+//! Project version string for Reusable_framework.
+FOUNDATION_EXPORT const unsigned char Reusable_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Reusable_framework/PublicHeader.h>
+
+
---
+++ Reusable_framework/Test.swift
@@ -0,0 +1,15 @@
+//
+//  Test.swift
+//  Reusable_framework
+//
+//  Created by BJIT on 2021/07/13.
+//
+
+import Foundation
+
+public class Test {
+    
+    public class func fetatureOne(){
+        print("fetatureOne")
+    }
+}
